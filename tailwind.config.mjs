2d3b4a8f4062134e576f1dd961a6c5f1d8474d1c
aufklarungjos <aufklarungjos@gmail.com>
2024-06-15 14:51:02 +0000

    config default background color daisyui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui"
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
 	plugins: [
-		daisyui,
-	]
+		require('daisyui'),
+	],
+	daisyui: {
+		base: false,
+	}
+
 }
