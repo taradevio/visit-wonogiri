40f42a8c97510750d79d75899115eb92754f84af
aufklarungjos <aufklarungjos@gmail.com>
2024-07-25 14:25:51 +0000

    add color and bg image

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      'reddish': '#ce4141',
+      'yellowish': '#f3ca40',
+      'white': '#f4eedb',
+      'blue': '#003049'
+    },
+    extend: {
+      backgroundImage: {
+        'nampu': "url('https://i.pinimg.com/736x/e9/f8/50/e9f850c6aeef44604f8307c4541796d3.jpg')"
+      }
+    }
+  },
   plugins: [require("daisyui")],
   daisyui: {
     base: false,
